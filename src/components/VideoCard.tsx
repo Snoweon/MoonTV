2281c3d9b96840b2ef4f225db431215bf7f2a8fa
Snoweon <125201825+Snoweon@users.noreply.github.com>
2026-03-07 22:30:43 +0800

    Update image source URL in VideoCard component

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -278,7 +278,7 @@ export default function VideoCard({
         {!isLoading && <ImagePlaceholder aspectRatio='aspect-[2/3]' />}
         {/* 图片 */}
         <Image
-          src={processImageUrl(actualPoster)}
+          src={processImageUrl(actualPoster).replace(/img\d+\.doubanio\.com/, 'img.doubanio.cmliussss.net')}
           alt={actualTitle}
           fill
           className='object-cover'
